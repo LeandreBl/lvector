3f53fc2d8d550d4a8fdcbab198400d3ad5157d49
Leandre <leandre.blanchard@epitech.eu>
2019-02-16 20:09:24 +0100

    [add] emplace_back and emplace macros [add] tests for emplace and emplace_back, assign, push_back and pop_back

--- lvector.h
+++ lvector.h
@@ -3,7 +3,7 @@
 
 # include <stdlib.h>
 
-#define LVECT_REALLOC_SIZE (5);
+#define LVECTOR_REALLOC_SIZE (5)
 
 #define lvector(type)     \
 struct                    \
@@ -14,105 +14,132 @@ struct                    \
   void (* destr)(type *); \
 }                         \
 
-#define lvect_destroy(vector) do {            \
-  lvect_clear(vector);                        \
-  free((vector).arr);                         \
-  memset(&vector, 0, sizeof(vector));         \
-} while (0)                                   \
+#define lvector_destroy(vector) do {  \
+  lvector_clear(vector);              \
+  free((vector).arr);                 \
+  memset(&vector, 0, sizeof(vector)); \
+} while (0)                           \
 
-#define lvect_type_size(vector) sizeof(*((vector).arr))
+#define lvector_type_size(vector) sizeof(*((vector).arr))
 
-#define lvect_create(vector, size, destructor) do { \
-  memset(&vector, 0, sizeof(vector));               \
-  (vector).rsize = lvect_type_size(vector) * size;  \
-  (vector).arr = malloc((vector).rsize);            \
-  if ((vector).arr == NULL)                         \
-    memset(&(vector), 0, sizeof(vector));           \
-  (vector).len = 0;                                 \
-  (vector).destr = destructor;                      \
-} while (0)                                         \
+#define lvector_create(vector, size, destructor) do { \
+  memset(&vector, 0, sizeof(vector));                 \
+  (vector).rsize = lvector_type_size(vector) * size;  \
+  (vector).arr = malloc((vector).rsize);              \
+  if ((vector).arr == NULL)                           \
+    memset(&(vector), 0, sizeof(vector));             \
+  (vector).len = 0;                                   \
+  (vector).destr = destructor;                        \
+} while (0)                                           \
 
-#define lvect_size(vector) ((vector).len)
+#define lvector_size(vector) ((vector).len)
 
-#define lvect_resize(vector, newsize) do {                                          \
-  if (newsize != (vector).rsize) {                                                  \
-    for (size_t i = newsize; i < (vector).len; ++i)                                 \
-      if ((vector).destr != NULL)                                                   \
-        (vector).destr(&(vector).arr[i]);                                           \
-    (vector).rsize = newsize;                                                       \
-    if (newsize < (vector).len)                                                     \
-      (vector).len = newsize;                                                       \
-    (vector).arr = realloc((vector).arr, (vector).rsize * lvect_type_size(vector)); \
-    if ((vector).arr == NULL)                                                       \
-      memset(&(vector), 0, sizeof(vector));                                         \
-  }                                                                                 \
-} while (0)                                                                         \
+#define lvector_resize(vector, newsize) do {                                          \
+  if (newsize != (vector).rsize) {                                                    \
+    for (size_t i = newsize; i < (vector).len; ++i)                                   \
+      if ((vector).destr != NULL)                                                     \
+        (vector).destr(&(vector).arr[i]);                                             \
+    (vector).rsize = newsize;                                                         \
+    if (newsize < (vector).len)                                                       \
+      (vector).len = newsize;                                                         \
+    (vector).arr = realloc((vector).arr, (vector).rsize * lvector_type_size(vector)); \
+    if ((vector).arr == NULL)                                                         \
+      memset(&(vector), 0, sizeof(vector));                                           \
+  }                                                                                   \
+} while (0)                                                                           \
 
-#define lvect_capacity(vector) ((vector).rsize / lvect_type_size(vector))
+#define lvector_capacity(vector) ((vector).rsize / lvector_type_size(vector))
 
-#define lvect_empty(vector) ((vector).len == 0)
+#define lvector_empty(vector) ((vector).len == 0)
 
-#define lvect_reserve(vector, reserve_size) do {  \
-  if (lvect_max_size(vector) < reserve_size)      \
-    lvect_resize(vector, reserve_size);           \
-} while (0)                                       \
+#define lvector_reserve(vector, reserve_size) do {                \
+  if (lvector_capacity(vector) < reserve_size)                    \
+    lvector_resize(vector, reserve_size * lvector_type_size(v));  \
+} while (0)                                                       \
 
-#define lvect_shrink_to_fit(vector) do {    \
-  lvect_resize(vector, lvect_size(vector)); \
-} while (0)                                 \
+#define lvector_shrink_to_fit(vector) do {      \
+  lvector_resize(vector, lvector_size(vector)); \
+} while (0)                                     \
 
-#define lvect_front(vector) (&(vector).arr[0])
+#define lvector_front(vector) (&(vector).arr[0])
 
-#define lvect_back(vector) ((vector.len != 0) ? (&(vector).arr[(vector).len - 1]) : (&(vector).arr[0]))
+#define lvector_back(vector) ((vector.len != 0) ? (&(vector).arr[(vector).len - 1]) : (&(vector).arr[0]))
 
-#define lvect_data(vector) ((vector).arr)
+#define lvector_data(vector) ((vector).arr)
 
-#define lvect_assign(vector, other_vector) ((vector) = (other_vector))
+#define lvector_assign(vector_dest, vector_src, copier) do {                  \
+  lvector_destroy(vector_dest);                                               \
+  lvector_create(vector_dest, lvector_size(vector_src), (vector_src).destr);  \
+  if (copier != NULL)                                                         \
+    for (size_t i = 0; i < lvector_size(vector_src); ++i)                     \
+      copier(&(vector_dest).arr[i], &(vector_src).arr[i]);                    \
+  else                                                                        \
+    for (size_t i = 0; i < lvector_size(vector_src); ++i)                     \
+      (vector_dest).arr[i] = (vector_src).arr[i];                             \
+  (vector_dest).len = lvector_size(vector_src);                               \
+} while (0)                                                                   \
 
-#define lvect_push_back(vector, new_item) do {                 \
-  if ((vector).len == (vector).rsize)                          \
-    lvect_resize(vector, (vector).rsize + LVECT_REALLOC_SIZE); \
-  (vector).arr[(vector).len] = new_item;                       \
-  ++(vector).len;                                              \
-} while (0)                                                    \
+#define lvector_push_back(vector, new_item) do {                    \
+  if ((vector).len == (vector).rsize)                               \
+    lvector_resize(vector, (vector).rsize + LVECTOR_REALLOC_SIZE);  \
+  (vector).arr[(vector).len] = new_item;                            \
+  ++(vector).len;                                                   \
+} while (0)                                                         \
 
-#define lvect_pop_back(vector) do {                 \
-  if (lvect_size(vector) != 0)                      \
-    lvect_erase(vector, (lvect_size(vector) - 1));  \
-} while (0)                                         \
+#define lvector_pop_back(vector) do {                   \
+  if (lvector_size(vector) != 0)                        \
+    lvector_erase(vector, (lvector_size(vector) - 1));  \
+} while (0)                                             \
 
-#define lvect_insert(vector, position, new_item) do {                                                                         \
-  if (position < lvect_size(vector)) {                                                                                        \
-    memmove(&(vector).arr[position + 1], &(vector).arr[position], (lvect_size(vector) - position) * lvect_type_size(vector)); \
-    (vector).arr[position] = new_item;                                                                                        \
-    ++(vector).len;                                                                                                           \
-  }                                                                                                                           \
-  else if (position == lvect_size(vector))                                                                                    \
-    lvect_push_back(vector, new_item);                                                                                        \
-} while (0)                                                                                                                   \
-
-#define lvect_erase(vector, position) do {                                                                                        \
-  if (position < lvect_size(vector)) {                                                                                            \
-    if ((vector).destr != NULL)                                                                                                   \
-      (vector).destr(&(vector).arr[position]);                                                                                    \
-    memmove(&(vector).arr[position], &(vector).arr[position + 1], (lvect_size(vector) - position - 1) * lvect_type_size(vector)); \
-    --(vector).len;                                                                                                               \
+#define lvector_insert(vector, position, new_item) do {                                                                           \
+  if (position < lvector_size(vector)) {                                                                                          \
+    memmove(&(vector).arr[position + 1], &(vector).arr[position], (lvector_size(vector) - position) * lvector_type_size(vector)); \
+    (vector).arr[position] = new_item;                                                                                            \
+    ++(vector).len;                                                                                                               \
   }                                                                                                                               \
+  else if (position == lvector_size(vector))                                                                                      \
+    lvector_push_back(vector, new_item);                                                                                          \
 } while (0)                                                                                                                       \
 
-#define lvect_erase_item(vector, item) do {       \
-  for (size_t i = 0; i < lvect_size(vector); ++i) \
-    if ((vector).arr[i] == item) {                \
-      lvect_erase(vector, i);                     \
-      break;                                      \
-    }                                             \
-} while (0)                                       \
+#define lvector_erase(vector, position) do {                                                                                          \
+  if (position < lvector_size(vector)) {                                                                                              \
+    if ((vector).destr != NULL)                                                                                                       \
+      (vector).destr(&(vector).arr[position]);                                                                                        \
+    memmove(&(vector).arr[position], &(vector).arr[position + 1], (lvector_size(vector) - position - 1) * lvector_type_size(vector)); \
+    --(vector).len;                                                                                                                   \
+  }                                                                                                                                   \
+} while (0)                                                                                                                           \
 
-#define lvect_clear(vector) do {                    \
-  if ((vector).destr != NULL)                       \
-    for (size_t i = 0; i < lvect_size(vector); ++i) \
-      ((vector).destr(&(vector).arr[i]));           \
-  (vector).len = 0;                                 \
+#define lvector_erase_item(vector, item) do {       \
+  for (size_t i = 0; i < lvector_size(vector); ++i) \
+    if ((vector).arr[i] == item) {                  \
+      lvector_erase(vector, i);                     \
+      break;                                        \
+    }                                               \
 } while (0)                                         \
+
+#define lvector_clear(vector) do {                    \
+  if ((vector).destr != NULL)                         \
+    for (size_t i = 0; i < lvector_size(vector); ++i) \
+      ((vector).destr(&(vector).arr[i]));             \
+  (vector).len = 0;                                   \
+} while (0)                                           \
+
+#define lvector_emplace_back(vector, function, ...) do {            \
+  if ((vector).len == (vector).rsize)                               \
+    lvector_resize(vector, (vector).rsize + LVECTOR_REALLOC_SIZE);  \
+  function(&(vector).arr[lvector_size(vector)], ##__VA_ARGS__);     \
+  ++(vector).len;                                                   \
+} while (0)                                                         \
+
+#define lvector_emplace(vector, position, function, ...) do {                                                                     \
+  if (position < lvector_size(vector)) {                                                                                          \
+    memmove(&(vector).arr[position + 1], &(vector).arr[position], (lvector_size(vector) - position) * lvector_type_size(vector)); \
+    function(&(vector).arr[lvector_size(vector)], ##__VA_ARGS__);                                                                 \
+    ++(vector).len;                                                                                                               \
+  }                                                                                                                               \
+  else if (position == lvector_size(vector))                                                                                      \
+    lvector_emplace_back(vector, function, ##__VA_ARGS__);                                                                        \
+} while (0)                                                                                                                       \
 
 #endif /* !_L_VECTOR_H_ */
--- tests/vector.c
+++ tests/vector.c
@@ -6,7 +6,7 @@ Test(lvector, create10)
 {
   lvector(int) v;
 
-  lvect_create(v, 10, NULL);
+  lvector_create(v, 10, NULL);
   cr_assert(v.arr != NULL);
   cr_assert(v.destr == NULL);
   cr_assert(v.len == 0);
@@ -17,47 +17,11 @@ Test(lvector, create0)
 {
   lvector(int) v;
 
-  lvect_create(v, 0, NULL);
+  lvector_create(v, 0, NULL);
   //cr_assert(v.arr != NULL); depends on malloc implementation :/
   cr_assert(v.destr == NULL);
   cr_assert(v.len == 0);
   cr_assert(v.rsize == 0);
-}
-
-Test(lvector, type_size)
-{
-  lvector(int) v;
-  cr_assert(lvect_type_size(v) == sizeof(int));
-  lvector(long) v1;
-  cr_assert(lvect_type_size(v1) == sizeof(long));
-  lvector(char *) v2;
-  cr_assert(lvect_type_size(v2) == sizeof(char *));
-  lvector(short) v3;
-  cr_assert(lvect_type_size(v3) == sizeof(short));
-}
-
-Test(lvector, capacity)
-{
-  lvector(int) v;
-
-  lvect_create(v, 10, NULL);
-  cr_assert(lvect_capacity(v) == 10);
-  lvect_destroy(v);
-  lvect_create(v, 0, NULL);
-  cr_assert(lvect_capacity(v) == 0);
-  lvect_destroy(v);
-  lvect_create(v, 1000, NULL);
-  cr_assert(lvect_capacity(v) == 1000);
-  lvect_destroy(v);
-}
-
-Test(lvector, empty)
-{
-  lvector(int) v;
-
-  lvect_create(v, 0, NULL);
-  cr_assert(lvect_empty(v) == true);
-  lvect_destroy(v);
 }
 
 static void free_str(char **pstr)
@@ -69,11 +33,249 @@ Test(lvector, destroy)
 {
   lvector(char *) v;
 
-  lvect_create(v, 20, free_str);
-  lvect_destroy(v);
+  lvector_create(v, 20, free_str);
+  for (size_t i = 0; i < 20; ++i) {
+    lvector_push_back(v, strdup("test"));
+  }
+  lvector_destroy(v);
   cr_assert(v.destr == NULL);
   cr_assert(v.len == 0);
   cr_assert(v.rsize == 0);
   cr_assert(v.arr == NULL);
 }
 
+Test(lvector, type_size)
+{
+  lvector(int) v;
+  cr_assert(lvector_type_size(v) == sizeof(int));
+  lvector(long) v1;
+  cr_assert(lvector_type_size(v1) == sizeof(long));
+  lvector(char *) v2;
+  cr_assert(lvector_type_size(v2) == sizeof(char *));
+  lvector(short) v3;
+  cr_assert(lvector_type_size(v3) == sizeof(short));
+}
+
+Test(lvector, capacity)
+{
+  lvector(int) v;
+
+  lvector_create(v, 10, NULL);
+  cr_assert(lvector_capacity(v) == 10);
+  lvector_destroy(v);
+  lvector_create(v, 0, NULL);
+  cr_assert(lvector_capacity(v) == 0);
+  lvector_destroy(v);
+  lvector_create(v, 1000, NULL);
+  cr_assert(lvector_capacity(v) == 1000);
+  lvector_destroy(v);
+}
+
+Test(lvector, empty)
+{
+  lvector(int) v;
+
+  lvector_create(v, 10, NULL);
+  cr_assert(lvector_empty(v) == true);
+  for (int i = 0; i < 10; ++i) {
+    lvector_push_back(v, i);
+  }
+  cr_assert(lvector_empty(v) == false);
+  for (int i = 0; i < 10; ++i) {
+    lvector_pop_back(v);
+  }
+  cr_assert(lvector_empty(v) == true);
+  lvector_destroy(v);
+}
+
+Test(lvector, reserve)
+{
+  lvector(int) v;
+
+  lvector_create(v, 0, NULL);
+  lvector_reserve(v, 10);
+  cr_assert(v.arr != NULL);
+  cr_assert(v.len == 0);
+  cr_assert(v.rsize >= sizeof(int) * 10);
+  lvector_reserve(v, 100);
+  cr_assert(v.arr != NULL);
+  cr_assert(v.len == 0);
+  cr_assert(v.rsize >= sizeof(int) * 100);
+  lvector_destroy(v);
+}
+
+Test(lvector, size)
+{
+  lvector(size_t) v;
+
+  lvector_create(v, 0, NULL);
+  cr_assert(lvector_size(v) == 0);
+  for (size_t i = 0; i < 100; ++i) {
+    cr_assert(lvector_size(v) == i);
+    lvector_push_back(v, i);
+  }
+  cr_assert(lvector_size(v) == 100);
+}
+
+Test(lvector, shrink_to_fit)
+{
+  lvector(int) v;
+
+  lvector_create(v, 100, NULL);
+  for (int i = 0; i < 50; ++i) {
+    lvector_push_back(v, i);
+  }
+  lvector_shrink_to_fit(v);
+  cr_assert(v.rsize >= v.len);
+  for (int i = 0; i < 50; ++i) {
+    lvector_pop_back(v);
+  }
+  lvector_shrink_to_fit(v);
+  cr_assert(v.rsize >= v.len);
+  lvector_destroy(v);
+}
+
+Test(lvector, front)
+{
+  lvector(int) v;
+  int *value;
+
+  lvector_create(v, 10, NULL);
+  lvector_push_back(v, 56);
+  lvector_push_back(v, 10000);
+  value = lvector_front(v);
+  *value = 12;
+  cr_assert(v.arr[0] == 12);
+  *lvector_front(v) = 556;
+  cr_assert(v.arr[0] == 556);
+  lvector_destroy(v);
+}
+
+Test(lvector, back)
+{
+  lvector(int) v;
+  int *value;
+
+  lvector_create(v, 10, NULL);
+  lvector_push_back(v, 1);
+  lvector_push_back(v, 2);
+  lvector_push_back(v, 12);
+  value = lvector_back(v);
+  *value = 3;
+  cr_assert(v.arr[v.len - 1] == 3);
+  *lvector_back(v) = 10000;
+  cr_assert(v.arr[v.len - 1] == 10000);
+  lvector_destroy(v);
+}
+
+Test(lvector, data)
+{
+  lvector(int) v;
+  int *arr;
+
+  lvector_create(v, 10, NULL);
+  for (int i = 0; i < 10; ++i)
+    lvector_push_back(v, i);
+  arr = lvector_data(v);
+  cr_assert(memcmp(arr, v.arr, sizeof(int) * v.len) == 0);
+  lvector_destroy(v);
+}
+
+static void string_copier(char **dest, char **src)
+{
+  *dest = strdup(*src);
+}
+
+Test(lvector, assign)
+{
+  lvector(char *) v;
+  lvector(char *) dup = { 0 };
+
+  lvector_create(v, 10, free_str);
+  lvector_push_back(v, strdup("Hello"));
+  lvector_push_back(v, strdup("Im"));
+  lvector_push_back(v, strdup("Leandre"));
+  lvector_assign(dup, v, string_copier);
+  cr_assert(dup.len == v.len);
+  cr_assert(dup.destr == v.destr);
+  cr_assert(dup.rsize <= v.rsize);
+  for (size_t i = 0; i < lvector_size(v); ++i) {
+    cr_assert(&dup.arr[i] != &v.arr[i]);
+    cr_assert(strcmp(dup.arr[i], v.arr[i]) == 0);
+  }
+  lvector_destroy(v);
+  lvector_destroy(dup);
+}
+
+Test(lvector, push_back)
+{
+  lvector(int) v;
+
+  lvector_create(v, 0, NULL);
+  for (size_t i = 0; i < 100; ++i) {
+    cr_assert(v.len == i);
+    lvector_push_back(v, i);
+    cr_assert(v.rsize >= i);
+  }
+  cr_assert(v.len == 100);
+  for (size_t i = 0; i < lvector_size(v); ++i)
+    cr_assert(v.arr[i] == (int)i);
+  lvector_destroy(v);
+}
+
+Test(lvector, pop_back)
+{
+  lvector(int) v;
+
+  lvector_create(v, 0, NULL);
+  for (size_t i = 0; i < 100; ++i)
+    lvector_push_back(v, i);
+  for (size_t i = 0; i < 20; ++i) {
+    cr_assert(v.len == 100 - i);
+    lvector_pop_back(v);
+  }
+  cr_assert(v.len == 80);
+  for (size_t i = 0; i < lvector_size(v); ++i)
+    cr_assert(v.arr[i] == (int)i);
+  lvector_destroy(v);
+}
+
+static void dup_str(char **s, const char *str)
+{
+  *s = strdup(str);
+}
+
+static void empty_str(char **s)
+{
+  *s = strdup("empty-str");
+}
+
+Test(lvector, emplace_back)
+{
+  lvector(char *) v;
+
+  lvector_create(v, 10, free_str);
+  lvector_emplace_back(v, dup_str, "Salut");
+  lvector_emplace_back(v, empty_str);
+  cr_assert(v.arr != NULL);
+  cr_assert(v.len == 2);
+  cr_assert(v.rsize >= 2);
+  cr_assert(strcmp(v.arr[0], "Salut") == 0);
+  cr_assert(strcmp(v.arr[1], "empty-str") == 0);
+  lvector_destroy(v);
+}
+
+Test(lvector, emplace)
+{
+  lvector(char *) v;
+
+  lvector_create(v, 10, free_str);
+  lvector_emplace(v, 0, dup_str, "Salut");
+  lvector_emplace(v, 0, empty_str);
+  cr_assert(v.arr != NULL);
+  cr_assert(v.len == 2);
+  cr_assert(v.rsize >= 2);
+  cr_assert(strcmp(v.arr[0], "Salut") == 0);
+  cr_assert(strcmp(v.arr[1], "empty-str") == 0);
+  lvector_destroy(v);
+}
